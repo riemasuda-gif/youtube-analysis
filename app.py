132f7c731b799e27a4a5a48f6a8da5450f49dd2d
riemasuda-gif <rie.masuda@stageon.jp>
2026-07-21 17:53:19 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -74,12 +74,12 @@ try:
         hover
     )
 
-    # 5. 重ね合わせ・動的タイトルの追加
+    # 5. 重ね合わせ・動的タイトルの追加（高さ850pxに拡張）
     chart = (
         alt.layer(chart_base, chart_hover, chart_event)
         .add_params(zoom_y)
         .properties(
-            height=500,
+            height=850,
             title=f"■ {y_axis_choice}の推移",
         )
     )
